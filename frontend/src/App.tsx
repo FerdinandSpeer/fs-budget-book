98a7302dfb26f8e591cc40f785b5b70390d32303
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-27 16:15:20 +0200

    some css in index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-
-
 function App() {
 
     return (
         <>
-        <h1> Project: Budget Book</h1>
+            <div className="nav-bar">
+                <ul>
+                    <li>Home</li>
+                    <li>Einnahmen</li>
+                    <li>Ausgaben</li>
+
+                </ul>
+            </div>
+            <div className={"total-income"}>
+                <p className={"p-text"}> Einnahmen Gesamt: </p>
+                <p className={"p-value"}> + 3.456 €</p>
+            </div>
+            <div className={"total-output"}>
+                <p className={"p-text"}> Ausgaben Gesamt: </p>
+                <p className={"p-value"}> - 2.456 €</p>
+
+            </div>
+            <div className={"total-summary"}>
+                <p className={"p-text"}> Aktueller Zwischenstand: </p>
+                <p className={"p-value"}> + 1.000 €</p>
+            </div>
         </>
     )
 }
